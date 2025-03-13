e7c477bace0ffd4228cadee120061acaf1f21988
1Gift3 <joshuaramaoka@gmail.com>
2025-03-13 17:59:47 +0200

    my first backend programs

---
+++ API_INTERGRATION/Real_time_translation_service/main.py
@@ -0,0 +1,22 @@
+# First Learned how to activate using Powershell after locating script in venv
+# .\activate.ps1
+# My issue i was not putting the Dot
+
+# Realised i ddnt have jinja which i had to install learn to install rather
+
+from fastapi import FastAPI, BackgroundTasks, HTTPException, Request, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware 
+
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Seting up for jinja2 templates with looks for our templates folder in the dir
+templates = Jinja2Templates(directory="templates")
+
+@app.get('/index', response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# recursive guard prohibts me from reading from this line
